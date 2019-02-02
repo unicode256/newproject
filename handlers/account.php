1ef09009106f63a7409909c5636b8f2a9929aa49
GLEB <unicode256@yandex.by>
2019-02-02 12:44:39 +0300

    just experiments 3

--- handlers/account.php
+++ handlers/account.php
@@ -7,8 +7,10 @@ if($module == 'registration' and $_POST['submit']){
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $row = mysqli_fetch_array(mysqli_query($CONNECT, "SELECT  FROM `users` WHERE `email` = '$email'"));
-    if($row[])
+    $row = mysqli_fetch_array(mysqli_query($CONNECT, "SELECT `email` FROM `users` WHERE `email` = '$email'"));
+    if($row['email']){
+        messageSend('Этот адрес уже существует');
+    }
     mysqli_query($CONNECT,"INSERT INTO `users` VALUES ('', '$name', '$email', '$password')");
 }
 ?>
