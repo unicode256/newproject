13c0660a8aeba6feb0c4040ccb16cc95fc8032ec
GLEB <unicode256@yandex.by>
2019-02-07 12:34:26 +0300

    working on appearance

--- pages/index.php
+++ pages/index.php
@@ -1,7 +1,24 @@
 <?php
     Ulogin(0);
-    echo 'Это главная странца' . '<br />';
-    messageShow();
+    Head('ANTIDATING', '../resources/styles/index-style.css');
 ?>
-<a href="registration">Регистрация</a><br />
-<a href="login">Вход</a>
+<div id="main">
+    <h1 id="logo"><img src="../resources/images/WhiteLogo.png" alt="ANTIDATING"></h1>
+    <p class="info info1">Знакомьтесь свободно.</p>
+    <p class="info info2">Никаких платных премиумов.</p>
+    <p class="info info3">Пользуйтесь всем функционалом нашей платформы абсолютно бесплатно</p>
+    <div class="buttons">
+        <a class="button button1" href="login">Войти</a>
+        <a class="button button2" href="registration">Зарегистрироваться</a>
+    </div>
+</div>
+<div class="footer">
+    <div class="fbuttons">
+        <a href="" class="fbutton fbutton1">Наши контакты</a>
+        <a href="" class="fbutton fbutton2">Наши правила</a>
+        <a href="" class="fbutton fbutton3">Наш блог</a>
+    </div>
+</div>
+<?php
+    Footer();
+?>
--- pages/login.php
+++ pages/login.php
@@ -1,16 +1,26 @@
 <?php
 Ulogin(0);
-Head('Вход', '#');
+Head('ANTIDATING | Вход', '../resources/styles/login-style.css');
 
 ?>
-<h1>Вход</h1>
-<form action="account/login" method="post">
-    Адрес почты:<br />
-    <input type="text" name="email"><br />
-    Пароль:<br />
-    <input type="password" name="password"><br />
-    <input name="submit" type="submit" value="Войти">
-</form>
+<div id="main">
+    <h1 id="logo"><img src="../resources/images/WhiteLogo.png" alt="ANTIDATING"></h1>
+    <form action="account/login" method="post">
+        <p id="bottom_line">Вход в учётную запись</p>
+        <p class="label">Введите ваш адрес эл. почты:</p>
+        <input type="text" name="email">
+        <p class="label">Введите ваш пароль:</p>
+        <input type="password" name="password"><br />
+        <input type="submit" value="Войти">
+    </form>
+</div>
+<div class="footer">
+    <div class="fbuttons">
+        <a href="" class="fbutton fbutton1">Наши контакты</a>
+        <a href="" class="fbutton fbutton2">Наши правила</a>
+        <a href="" class="fbutton fbutton3">Наш блог</a>
+    </div>
+</div>
 <?php
 messageShow();
 Footer();
